320161a778e84c0df86700eb9f52d465b41d0caa
JiachenLiu09 <37533434+JiachenLiu09@users.noreply.github.com>
2019-09-12 14:37:23 +1000

    fix error

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,31 @@
 import * as vscode from 'vscode';
 import { TextDecoder } from 'util';
 
-export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('extension.addComment', () => {
-		vscode.window.showInputBox().then(result => addComment(getText('file://' + result)));
-	});
-
-	context.subscriptions.push(disposable);
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.addComment', () => {
+        vscode.window.showInputBox().then(result => getText(result));
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function getText(uri) {
+    if (uri) {
+        let u = vscode.Uri.file(uri, true);
+        vscode.workspace.openTextDocument(u).then((document) => {
+            let text = document.getText();
+            if(text) {
+                let editor = vscode.window.activeTextEditor;
+                if (editor) {
+                    let l = editor.selection.active.line;
+                    let c = editor.selection.active.character;
+                    editor.edit(editBuilder => {
+                        editBuilder.replace(new vscode.Range(new vscode.Position(l, c), new vscode.Position(l, c++)), "/* " + text + " */");
+                    });
+                }
+            }
+        });
+    }
 }
 
-function getText(uri : any) {
-	if(uri) {
-		let u = vscode.Uri.parse(uri, true);
-		console.log(u);
-		let text;
-		vscode.workspace.openTextDocument(u).then((document) => {
-			console.log(document);
-			text = document.getText();
-		});
-		console.log(text);
-		return text;
-	}
-}
-
-function addComment(contain : any) {
-	if(contain) {
-		let editor = vscode.window.activeTextEditor; 
-		if(editor) {
-			let l = editor.selection.active.line;
-			let c = editor.selection.active.character;
-			editor.edit(editBuilder => {
-				editBuilder.replace(new vscode.Range(new vscode.Position(l, c), new vscode.Position(l, c++)), "// " + contain);
-			});
-		}
-	}
-}
-
-
-export function deactivate() {}
+function deactivate() { }
+exports.deactivate = deactivate;
